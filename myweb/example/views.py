c4770595c3d1a3529cdec93ba9a4e88b574d9328
bea816 <kimnadon@naver.com>
2024-09-11 20:42:55 +0900

    2주차 스터디

--- myweb/example/views.py
+++ myweb/example/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 
+# mixins
+from rest_framework import generics
+from rest_framework import mixins
+
 from .models import *
 from .serializers import *
 
@@ -77,3 +81,48 @@ class BookAPI(APIView):
         book = get_object_or_404(Book, bid=bid)
         serializer = BookSerializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+"""
+mixins: 동일 시리얼라이저 중복 사용 제거
+"""
+class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer # 사용할 시리얼라이저 지정
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class BookAPIMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,mixins.DestroyModelMixin,  generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid' # pk
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+"""
+mixins 세트
+"""
+class BooksAPIGenerics(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookAPIGenerics(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
+
+"""
+viewset & router
+"""
+from rest_framework import viewsets
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
